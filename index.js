6730a8bb27063de174db8255adcc253d2d00c033
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-05 19:00:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#000000";
+
+const moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+    if (left > 0) {
+        dodger.style.left = `${left - 5}px`;
+    }
+}
+
+const moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+    if (left < 360) {
+        dodger.style.left = `${left + 5}px`;
+    }
+}
+
+document.addEventListener("keydown", e => {
+    if(e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+    else if(e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
